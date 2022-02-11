cc67d82b93545fe31f54f5c8387d1c7fabbb993b
Maxim <yourstenex@gmail.com>
2022-02-11 14:19:30 +0300

    main, tailwind and manifest.json setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import "./App.css";
+import "./styles/main.css";
 
 const App = () => {
-  return <h1>Hello, world!</h1>;
+  return <h1 className="font-bold">Hello, world!</h1>;
 };
 
 export default App;
